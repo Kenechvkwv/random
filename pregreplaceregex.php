3681aa6735d9ebfc6fb7b509a3e47211b8780fe3
kenechvkwv <ubesiekene@gmail.com>
2021-07-19 20:02:21 +0100

    new

--- pregreplaceregex.php
+++ pregreplaceregex.php
@@ -1,8 +1,12 @@
 <?php
-$text = "I love to design websites, and also design UI for mobile apps";
-//this replaces the word "design" with a green tinted "develop"
-$text = preg_replace("/design/", '<span style="background:green">develop</span>', $text);
-//echoes the new content of the $text variable
-echo $text;
+$text = "Japan is a beautiful country with a rich history. I love Japan. Another country with a rich history is Japan";
+//Using the $limit to indicate how many times the tagetted word:Japa, should be replaced. 
+$limit = 2;
+//the $count can be used to indicate the number of times the preg_replace is carried out
+$count = 0;
+//this replaces the word "Japan" with a pink coloured "Brazil"
+$text = preg_replace("/Japan/", '<span style="color:deeppink; font-weight: bold;">Brazil</span>',$text, $limit, $count);
+//echoes the new content of the $text variable as well as counts the number of replacements
+echo $text . "<br>".$count." words were replaced"."<br>" ;
 
 ?>
